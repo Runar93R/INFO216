2e2387ad74f6f1e73410324e7e0ae761b8a4e97b
egilmela <egilmela@gmail.com>
2020-02-11 00:39:44 -0500

    installerte venv
    +fungerende kode
    +eksempeltekst

--- venv/run.py
+++ venv/run.py
@@ -15,14 +15,13 @@ import nb_core_news_sm
 from tqdm import tqdm_notebook as tqdm
 from pprint import pprint
 
-nrk = pd.read_csv('nrk.csv')
+nrk = pd.read_csv('include/nrk.csv', sep='\t', skiprows=0, header=0)
 newest_doc = nrk['content']
 
-
-nlp = spacy.load("nb")
+nlp = spacy.load("nb_core_news_sm")
 
 # My list of stop words.
-stop_list = ["Mrs.", "Ms.", "say", "WASHINGTON", "'s", "Mr.", ]
+stop_list = ["sier"]
 
 # Updates spaCy's default stop words list with my additional words.
 nlp.Defaults.stop_words.update(stop_list)
@@ -31,3 +30,47 @@ nlp.Defaults.stop_words.update(stop_list)
 for word in STOP_WORDS:
     lexeme = nlp.vocab[word]
     lexeme.is_stop = True
+
+
+def lemmatizer(doc):
+    # This takes in a doc of tokens from the NER and lemmatizes them.
+    # Pronouns (like "I" and "you" get lemmatized to '-PRON-', so I'm removing those.
+    doc = [token.lemma_ for token in doc if token.lemma_ != '-PRON-']
+    doc = u' '.join(doc)
+    return nlp.make_doc(doc)
+
+
+def remove_stopwords(doc):
+    # This will remove stopwords and punctuation.
+    # Use token.text to return strings, which we'll need for Gensim.
+    doc = [token.text for token in doc if token.is_stop != True and token.is_punct != True]
+    return doc
+
+
+# The add_pipe function appends our functions to the default pipeline.
+nlp.add_pipe(lemmatizer, name='lemmatizer', after='ner')
+nlp.add_pipe(remove_stopwords, name="stopwords", last=True)
+
+doc_list = []
+# Iterates through each article in the corpus.
+for doc in tqdm(newest_doc):
+    # Passes that article through the pipeline and adds to a new list.
+    pr = nlp(doc)
+    doc_list.append(pr)
+
+# Creates, which is a mapping of word IDs to words.
+words = corpora.Dictionary(doc_list)
+
+# Turns each document into a bag of words.
+corpus = [words.doc2bow(doc) for doc in doc_list]
+
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=words,
+                                           num_topics=10,
+                                           random_state=2,
+                                           update_every=1,
+                                           passes=10,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+pprint(lda_model.print_topics(num_words=10))
