cf29800428f3464fed8fd635b01eec5ae3cc1720
egilmela <egilmela@gmail.com>
2020-02-11 00:18:01 -0500

    installerte venv

--- venv/run.py
+++ venv/run.py
@@ -14,3 +14,20 @@ import nb_core_news_sm
 
 from tqdm import tqdm_notebook as tqdm
 from pprint import pprint
+
+nrk = pd.read_csv('nrk.csv')
+newest_doc = nrk['content']
+
+
+nlp = spacy.load("nb")
+
+# My list of stop words.
+stop_list = ["Mrs.", "Ms.", "say", "WASHINGTON", "'s", "Mr.", ]
+
+# Updates spaCy's default stop words list with my additional words.
+nlp.Defaults.stop_words.update(stop_list)
+
+# Iterates over the words in the stop words list and resets the "is_stop" flag.
+for word in STOP_WORDS:
+    lexeme = nlp.vocab[word]
+    lexeme.is_stop = True
